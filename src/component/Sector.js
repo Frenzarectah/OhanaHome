2619e9f5b04fc9054cce6d6ee73ff21cb2ea4e92
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2023-04-15 21:07:49 +0200

    minor fix

--- src/component/Sector.js
+++ src/component/Sector.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sector = ()=>{
     return(
-        <h1 className="w-full m-5 flex justify-center items-center font-Roboto">
+        <h1 className="w-full m-5 flex justify-center items-center content-center font-Roboto">
             Sito in costruzione, per prenotazioni e/o informazioni chiamare<br/>
             GIORGIA 391/7118040<br/>
             GABRIELLA 389/5319347
