3c5ef8c87808a89bde54185e5d6f6bc8616a3504
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2023-04-15 21:10:49 +0200

    minor fix

--- src/component/Sector.js
+++ src/component/Sector.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Sector = ()=>{
     return(
-        <h1 className="w-full m-5 flex justify-center items-center content-center font-Roboto">
-            Sito in costruzione, per prenotazioni e/o informazioni chiamare<br/>
-            GIORGIA 391/7118040<br/>
-            GABRIELLA 389/5319347
+        <h1 className="w-full flex justify-center items-center content-center font-Roboto">
+            <div className="w-[50%]">
+                Sito in costruzione, per prenotazioni e/o informazioni chiamare<br/>
+                GIORGIA 391/7118040<br/>
+                GABRIELLA 389/5319347
+            </div>
         </h1>
     )
 }
