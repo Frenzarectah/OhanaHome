412bdb2e1a08561e0fafe42ac08a6e82b5b8eb36
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2023-05-08 19:23:38 +0200

    added the responsive menu

--- src/pages/Template.js
+++ src/pages/Template.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "../component/Head";
-import NavBar from "../component/NavBar";
+import NavBar from "../component/Menu";
 import Sector from "../component/Sector";
 import Footer from "../component/Footer";
 
