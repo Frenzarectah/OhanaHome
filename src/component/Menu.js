f89dd5af4a9d85f085d7142c5200a6f87c8d16b8
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2023-05-10 19:28:58 +0200

    improving some feat

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -19,7 +19,7 @@ const Navbar = ()=>{
             <div className="w-full fixed bg-white h-16 flex items-center">
                 <img src={logo} alt="logo" className="w-16 m-2"/>
                 <ul className="rounded-lg bg-white/75 md:bg-transparent flex flex-col text-justify absolute top-12 right-0 md:static md:flex-row md:justify-around w-5/10 md:w-3/5 text-[#6EB0FE] font-Roboto">
-                    <a className={appear?"text-2xl m-3 font-bold md:text-lg":"hidden"}  href="#home">Home</a>
+                    <a className={appear?"text-2xl m-3 font-bold md:text-lg":"hidden"} onClick={()=>setAppear(false)}  href="#home">Home</a>
                     <a className={appear?"text-2xl m-3 font-bold md:text-lg":"hidden"} href="#photo">Photos</a>
                     <a className={appear?"text-2xl m-3 font-bold md:text-lg":"hidden"} href="#contact">Contact</a>
                     <a className={appear?"text-2xl m-3 font-bold md:text-lg":"hidden"} href="#contact">Book!</a>
--- src/component/Sector.js
+++ src/component/Sector.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sector = ()=>{
     return(
-        <h1 id="home" className="w-full font-Roboto text-md flex flex-col md:flex-row">
+        <main id="home" className="w-full font-Roboto text-md flex flex-col md:flex-row">
             <div className="w-full md:w-[75%] my-5 p-5">
                 <p>Ohana home è il luogo ideale se vuoi goderti qualche giorno di relax nella bellissima città storica di Verona.
                 Situato in una zona strategica della città, garantisce zero traffico, tranquillità e la vicinanza alle principali arterie
@@ -23,7 +23,7 @@ const Sector = ()=>{
                 <p className="font-Italianno text-2xl">Ohana Staff</p>
             </div>
             <iframe title="mappa" className="w-[25%]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1135.7214626567416!2d10.966019165182663!3d45.419111733240925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4781e1d60945dbc3%3A0x6055ec6ef9def2a4!2sVia%20Giuseppe%20Ruffoni%2C%2037136%20Verona%20VR!5e0!3m2!1sit!2sit!4v1681735475935!5m2!1sit!2sit" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </h1>
+        </main>
     )
 }
 export default Sector
