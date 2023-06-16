ea9169dd447638ff2bd3d0b80b89c9f73627e433
Francesco Sorrentino <sorrentino.francesco87@gmail.com>
2023-06-16 20:03:57 +0200

    bugfix gallery

---
+++ src/store.js
@@ -0,0 +1,76 @@
+import photo1 from './assets/1.JPG'
+import photo2 from './assets/2.JPG'
+import photo3 from './assets/3.JPG'
+import photo4 from './assets/4.JPG'
+import photo5 from './assets/5.JPG'
+import photo6 from './assets/6.JPG'
+import photo7 from './assets/7.JPG'
+import photo8 from './assets/8.JPG'
+import photo9 from './assets/9.JPG'
+import photo10 from './assets/10.JPG'
+import photo11 from './assets/11.jpeg'
+import photo12 from './assets/12.jpeg'
+import photo13 from './assets/13.jpeg'
+import photo14 from './assets/14.jpeg'
+
+let i = 0;
+const photos = [
+        {key:i++,
+         classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+         url: `${photo1}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo2}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo3}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo4}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo5}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo6}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo7}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo8}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo9}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo10}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo11}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo12}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo13}`
+        },
+        {key:i++,
+        classes: "gal object-cover rounded-md object-center overflow-hidden w-[250px] h-[250px] md:w-[300px] md:h-[250px]",
+        url: `${photo14}`
+        }
+]
+
+export default photos
